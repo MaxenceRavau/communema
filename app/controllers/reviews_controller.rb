e0a51eac0b5804acd60b761e12437aa652cea9ef
nafti ladhiri <ladhiri.nafti@gmail.com>
2022-11-29 15:19:55 +0100

    on avance

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,28 @@
 class ReviewsController < ApplicationController
+  def index
+    @reviews = policy_scope(Review).all
+  end
+
+  def new
+    @review = Review.new
+    authorize @review
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.movie = @movie
+    @review.user = current_user
+    authorize @review
+    if @review.save
+      redirect_to reviews_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
+  end
 end
