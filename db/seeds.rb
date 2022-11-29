a9abdd2c18e96846f7227750ff2400997142b3e0
maxence ravau <maxenceravau@gmail.com>
2022-11-29 12:12:03 +0100

    controller movie added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+@titanic = Movie.create(title: "Titanic", synopsis: "le bateau coule", genre: "drame", release_date: 1995, duration: 120, director: "James Cameron", market_rating: 5)
+
+@avatar = Movie.create(title: "avatar", synopsis: "blue guys", genre: "action", release_date: 2009, duration: 120, director: "James Cameron", market_rating: 4)
+
+puts "seed is done"
