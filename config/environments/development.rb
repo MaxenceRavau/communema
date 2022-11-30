1eb449d97fd97ef258afd02dd9a4bcc6806f9d77
nafti ladhiri <ladhiri.nafti@gmail.com>
2022-11-30 11:06:07 +0100

    je reviens asap

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
   config.hosts.clear
   config.hosts << /[a-z0-9-.]+\.eu\.ngrok\.io/
   config.hosts << "localhost"
+  config.hosts << "[::1]"
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
