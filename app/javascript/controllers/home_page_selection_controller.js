3d0b45a96c07db00fc18c4872e3d14f351b4582d
maxence ravau <maxenceravau@gmail.com>
2022-12-01 12:31:59 +0100

    added the js

--- app/javascript/controllers/home_page_selection_controller.js
+++ app/javascript/controllers/home_page_selection_controller.js
@@ -3,10 +3,12 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="home-page-selection"
 export default class extends Controller {
   //ajouter un target sur mes deux containers : un pour suggestion et un pour sortie (par défaut sortie hidden)
-  static targets = ["movieSuggestionInput", "movieSortiesInput", "movieSuggestionLabel", "movieSortiesLabel"]
+  static targets = ["movieSuggestionInput", "movieSortiesInput", "movieSuggestionLabel", "movieSortiesLabel", "suggestion", "sortiesSemaine"]
   connect() {
+    console.log("Hello")
 
   }
+
   updateContent() {
 //ajouter un target sur mes deux containers : un pour suggestion et un pour sortie (par défaut sortie hidden)
 // dans les conditions, dire que si c'est check, j'ajoute ou non la class hidden sur tout mon container ContainerSortieTarget...)
@@ -14,10 +16,15 @@ export default class extends Controller {
     if (this.movieSuggestionInputTarget.checked){
     this.movieSuggestionLabelTarget.classList.add("selected-movies-title")
     this.movieSortiesLabelTarget.classList.remove("selected-movies-title")
+    this.sortiesSemaineTarget.classList.add("hidden-div")
+    this.suggestionTarget.classList.remove("hidden-div")
+
 
   }else {
     this.movieSuggestionLabelTarget.classList.remove("selected-movies-title")
     this.movieSortiesLabelTarget.classList.add("selected-movies-title")
+    this.suggestionTarget.classList.add("hidden-div")
+    this.sortiesSemaineTarget.classList.remove("hidden-div")
     }
   }
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_103745) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_095829) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -84,13 +84,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_103745) do
     t.string "title"
     t.text "synopsis"
     t.string "genre"
-    t.integer "release_date"
     t.integer "duration"
     t.string "director"
     t.float "market_rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "tmdb_id"
+    t.date "release_date"
   end
 
   create_table "reviews", force: :cascade do |t|
