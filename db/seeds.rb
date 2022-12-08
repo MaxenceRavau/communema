0a8f5959a33b83eba7a8e36611bb743f10d34cc1
Hamza Triqui <triqui.hamza@gmail.com>
2022-12-08 15:22:18 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 require "json"
 require "open-uri"
 
+puts "destroying..."
+
 Message.destroy_all
 Sharing.destroy_all
 Cinema.destroy_all
@@ -9,12 +11,14 @@ Review.destroy_all
 User.destroy_all
 Movie.destroy_all
 
-puts "destroying..."
-
+puts "DB cleared!!!"
 # ---- Users ----
 
+puts "creation des users..."
 ParsingStudents.new.call
 
+
+puts "creation des follows..."
 User.all.each do |user|
   User.where.not(id: user.id).each do |buddy|
     Follow.create(
@@ -47,6 +51,7 @@ serialized_salles = File.read(filepath)
 
 @salles.each do |salle|
   next if salle["fields"]["unite_urbaine_2010"] != "Paris"
+  next if salle["fields"]["entrees_2020"] < 50000
 
   puts "Creation du ciné #{salle["fields"]["nom"]}..."
   Cinema.create!(
@@ -76,14 +81,6 @@ end
 
 # tmdb_ids = Movie.limit(10).pluck(:tmdb_id)
 
-tmdb_ids = ["436270", "505642", "791177", "615952", "837881", "944303", "872709", "998783", "971594", "998783", "899112", "785980", "664469", "918044","865498"]
-
-# ---- On récupère les movies qui correspondent aux ids ci-dessus. Instances de movies ----
-
-movies = Movie.where(tmdb_id: tmdb_ids)
-
-# ---- On crée les séances types ----
-
 session_plannings = [
   ['14:00', '16:30', '19:00', '21:30'],
   ['15:00', '17:30', '20:00', '22:30'],
@@ -91,11 +88,73 @@ session_plannings = [
   ['13:30', '16:15', '19:55']
 ]
 
-# ---- On appelle les cinémas qu'on a parsé plus haut dans un array cinemas ----
+# ---- Film à rejoindre - start ----
 
-cinemas = Cinema.all
+movie_to_join = Movie.find_by(tmdb_id: "892515")
 
-# ---- On crée des commentaires type  ----
+sessions = []
+
+Cinema.all.each do |cinema|
+  session_plannings.sample.each do |starting_hour|
+    4.times do |i|
+      puts "Création d'une session"
+      start_at = Time.parse(Date.today.strftime('%y/%m/%d') + " #{starting_hour}") + i.days
+      session = Session.create(movie: movie_to_join, cinema: cinema, start_at: start_at)
+      sessions.push(session) if i == 1
+    end
+  end
+end
+
+max = User.find_by(email: "maxence.ravau@mail.com")
+max_friends = max.followed_users
+chosen_friend = max_friends.sample
+
+other_friends = max_friends.where.not(id: chosen_friend.id)
+
+attendees_users = other_friends.shuffle.sample(3)
+
+sharing = Sharing.create!(session: sessions.first, user: chosen_friend, description: 'Qui est chaud pour aller regarder Mascarade après la teuf de fin de batch?')
+
+attendees_users.each do |attendee_user|
+  Attendee.create(user: attendee_user, sharing: sharing)
+end
+
+Message.create(
+  content: 'Qui est chaud pour aller regarder Mascarade après la teuf de fin de batch?',
+  user: chosen_friend,
+  sharing: sharing
+)
+
+Message.create(
+  content: 'Franchement pourquoi pas, je kiffe Pierre Niney',
+  user: attendees_users.first,
+  sharing: sharing
+)
+
+Message.create(
+  content: 'Why not mais ça risque de finir tard la teuf du Wagon?',
+  user: attendees_users.second,
+  sharing: sharing
+)
+
+Message.create(
+  content: 'Pas grave on y va à la sortie justement',
+  user: attendees_users.first,
+  sharing: sharing
+)
+
+Message.create(
+  content: "Maxence allez chauffe toi mec, tu m'as dit que tu voulais le voir",
+  user: attendees_users.third,
+  sharing: sharing
+)
+
+# ---- Film à rejoindre - end ----
+
+# ---- Film à partager - start ----
+
+movie_to_share = Movie.find_by(tmdb_id: "872709")
+friends = max_friends.shuffle.first(3)
 
 comments = [
   "Bon film, allez y les amis, il vaut le coup d'être regardé",
@@ -105,36 +164,84 @@ comments = [
   "A voir absolument les potes"
 ]
 
-# ---- Création de sessions historiques ----
+friends.each_with_index do |friend, index|
+  Review.create(movie: movie_to_share, rating: [4, 5].sample, user: friend, comment: comments[index])
+end
 
-cinema = Cinema.first
-movies.each do |movie|
-  start_at = Time.parse(Date.today.strftime('%y/%m/%d') + " 20:00") - 7.days
-  # session = Session.create(movie: movie, cinema: cinema, start_at: start_at)
-  User.all.shuffle.first(rand(6..10)).each do |user|
-    Review.create(movie: movie, rating: [2, 3, 4, 5].sample, user: user, comment: comments.sample)
+# ---- Film à partager - end ----
+
+# ---- Sharing random - start ----
+
+tmdb_ids = ["436270", "505642", "791177", "615952", "944303", "998783", "971594", "899112", "785980", "664469", "918044","865498"]
+
+shared_movies = Movie.where(tmdb_id: tmdb_ids).sample(4)
+
+chosen_cinema = Cinema.all.sample
+
+chosen_sharings = []
+
+shared_movies.each do |shared_movie|
+  chosen_session = Session.create(movie: shared_movie, cinema: chosen_cinema, start_at: Date.tomorrow)
+  sharing_creator = friends.sample
+  chosen_sharing = Sharing.create(session: chosen_session, user: sharing_creator, description: 'Trop envie de voir ce film')
+  attendee_users = friends.reject { |user| user == sharing_creator }.sample(rand(1..4))
+  attendee_users.each do |user|
+    Attendee.create(user: user, sharing: chosen_sharing)
   end
 end
 
-# ---- Dans chaque cine, pour chaque film, on crée une session ----
+# chose_sharings.each do |chosen_sharing|
+#   Attendee.create(user: attendees_users.sample, sharing: chosen_sharing)
+# end
 
-sessions = []
+# cinemas = Cinema.all
 
-cinemas.each do |cinema|
-  movies.each do |movie|
-    session_plannings.sample.each do |starting_hour|
-      4.times do |i|
-        puts "Création d'une session"
-        start_at = Time.parse(Date.today.strftime('%y/%m/%d') + " #{starting_hour}") + i.days
-        sessions.push(Session.create(movie: movie, cinema: cinema, start_at: start_at))
-      end
-    end
-  end
-end
+# # ---- On crée des commentaires type  ----
 
-# ---- On crée deux sharings pour user.first ----
+# chosen_comments = [
+#   "Bon film, allez y les amis, il vaut le coup d'être regardé",
+#   "J'ai bien aimé, je pense que je devrais le revoir une deuxième fois pour tout comprendre",
+#   "Top, ça faisait un bail que je n'étais pas allé au ciné",
+#   "Allez y sans hésiter !",
+#   "A voir absolument les potes"
+# ]
 
-shared_sessions = sessions.shuffle.first(2)
-shared_sessions.each do |session|
-  Sharing.create!(session: session, user: User.first, description: 'Trop envie de voir ce truc')
-end
+# ---- Création de sessions historiques pour la liste de films ----
+
+# cinema = Cinema.first
+# chosen_movies.each do |chosen_movie|
+#   start_at = Time.parse(Date.today.strftime('%y/%m/%d') + " 20:00") - 7.days
+#   User.all.shuffle.first(rand(6..10)).each do |user|
+#     Review.create(movie: chosen_movie, rating: [4, 5].sample, user: user, comment: chosen_comments.sample)
+#   end
+# end
+
+# movies.each do |movie|
+#   start_at = Time.parse(Date.today.strftime('%y/%m/%d') + " 20:00") - 7.days
+#   User.all.shuffle.first(rand(6..10)).each do |user|
+#     Review.create(movie: movie, rating: [3, 4, 5].sample, user: user, comment: comments.sample)
+#   end
+# end
+
+# # ---- Dans chaque cine, pour chaque film, on crée une session ----
+
+# sessions = []
+
+# cinemas.each do |cinema|
+#   movies.each do |movie|
+#     session_plannings.sample.each do |starting_hour|
+#       4.times do |i|
+#         puts "Création d'une session"
+#         start_at = Time.parse(Date.today.strftime('%y/%m/%d') + " #{starting_hour}") + i.days
+#         sessions.push(Session.create(movie: movie, cinema: cinema, start_at: start_at))
+#       end
+#     end
+#   end
+# end
+
+# # ---- On crée deux sharings pour user.first ----
+
+# shared_sessions = sessions.shuffle.first(2)
+# shared_sessions.each do |session|
+#   Sharing.create!(session: session, user: User.first, description: 'Trop envie de voir ce truc')
+# end
